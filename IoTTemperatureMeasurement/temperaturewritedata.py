86782399d6dca47cde2a726573ccaf4f9346e448
villeava <34127126+villeava@users.noreply.github.com>
2018-12-22 14:34:24 +0200

    Changes for telldus data use

--- IoTTemperatureMeasurement/temperaturewritedata.py
+++ IoTTemperatureMeasurement/temperaturewritedata.py
@@ -1,7 +1,11 @@
+
+
 import time
 import json
 import sys
 import datetime
+sys.path.append('/home/pi/IoTTicket-PythonLibrary')
+
 from iotticket.models import device
 from iotticket.models import criteria
 from iotticket.models import deviceattribute
@@ -15,31 +19,27 @@ password = data["password"]
 deviceId = data["deviceId"]
 baseurl = data["baseurl"]
 
-while 1:
-	#create client object
-	c = Client(baseurl,username,password)	
-	#read the content of the sensor
-	tempfile = open("/sys/bus/w1/devices/28-000006b19b27/w1_slave")
-	text = tempfile.read()
-	tempfile.close()
-	#split to get the second line temperature data
-	tempdata = text.split("\n")[1].split(" ")[9]
-	#get the actual number of the temperature
-	temperature = float(tempdata[2:])
-	#divided by 1000 to get real temperature degree
-	temperature = temperature/1000
-	#create datanodesvalue object and call the set functions
-	nv = datanodesvalue()
-	nv.set_name("Sensor temperature")
-	nv.set_path("tempVal")
-	nv.set_dataType("double")
-	nv.set_unit("deg")
-	#set the value of the node with the temperature value
-	nv.set_value(temperature)
-	#set the timestamp as now
-	nv.set_timestamp(c.dttots(datetime.datetime.now()))
-	#call writedata function
-	c.writedata(deviceId, nv)
-	print("v: " + str(nv.get_value()) +"\nts: " + str(nv.get_timstamp()) + "\n")
-	#the program will run every 2 seconds
-	time.sleep(2)
+#create client object
+c = Client(baseurl,username,password)	
+tempfile = open("/home/pi/IoT-ticket/" + sys.argv[2] + ".data")
+text = tempfile.read()
+tempfile.close()	
+temperature = float(text)
+
+nv = datanodesvalue()
+nv.set_name(sys.argv[2])
+nv.set_path("TelldusNetwork")
+nv.set_dataType("double")
+
+nv.set_unit("deg")
+
+	
+#set the value of the node with the temperature value
+nv.set_value(temperature)
+#set the timestamp as now
+nv.set_timestamp(c.dttots(datetime.datetime.now()))
+#call writedata function
+c.writedata(deviceId, nv)
+print("v: " + str(nv.get_value()) +"\nts: " + str(nv.get_timstamp()) + "\n")
+#the program will run every 2 seconds
+	
